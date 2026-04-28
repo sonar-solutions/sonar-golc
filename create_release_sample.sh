60ef002a1e03ae4903a2a06f303656325720f210
fabio-gos-sonarsource <fabio.gos@sonarsource.com>
2026-04-28 21:41:00 +0200

    Adjusting release script and removing docker docs

--- create_release_sample.sh
+++ create_release_sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export TAG="XXXXXX" # Release TAG in GitHub
-export Release1="XXXXXX" # Release Number
+export TAG="ver2.0" # Release TAG in GitHub
+export Release1="ver2.0" # Release Number
 export buildpath="XXXXXXX"  # Replace with the path where the release files are located
 
 CMD=`PWD`
